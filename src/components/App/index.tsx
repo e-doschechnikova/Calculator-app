a37458f3517b6664217cca05ec8ef5b72a4f9685
e-doschechnikova <evadrobakova@gmail.com>
2023-03-06 19:45:39 +0300

    fix styles and add buttons

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../Button";
 import "./App.scss";
 
 const App = () => {
+  const [value, setValue] = useState<string>("0");
   return (
     <div className="App">
       <div className="top">17:38</div>
-      <div className="display">0</div>
+      <div className="display">{value}</div>
       <div className="buttons">
-        <Button content={"AC"} />
-        <Button content={"±"} />
-        <Button content={"%"} />
-        <Button content={"÷"} />
+        <Button content={"AC"} type={"function"} />
+        <Button content={"±"} type={"function"} />
+        <Button content={"%"} type={"function"} />
+        <Button content={"÷"} type={"operator"} />
         <Button content={"7"} />
         <Button content={"8"} />
         <Button content={"9"} />
-        <Button content={"×"} />
+        <Button content={"×"} type={"operator"} />
         <Button content={"4"} />
         <Button content={"5"} />
         <Button content={"6"} />
-        <Button content={"−"} />
+        <Button content={"−"} type={"operator"} />
         <Button content={"1"} />
         <Button content={"2"} />
         <Button content={"3"} />
-        <Button content={"+"} />
+        <Button content={"+"} type={"operator"} />
         <Button content={"0"} />
         <Button content={"."} />
-        <Button content={"="} />
+        <Button content={"="} type={"operator"} />
       </div>
       <div className="bottom"></div>
     </div>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,12 @@ import "./Button.scss";
 
 type ButtonPropsType = {
   content: string;
+  type?: string;
 };
 
-export const Button: React.FC<ButtonPropsType> = ({ content }) => {
+export const Button: React.FC<ButtonPropsType> = ({ content, type }) => {
   return (
-    <button className={`Button ${content === "0" ? "zero" : ""}`}>
+    <button className={`Button ${content === "0" ? "zero" : ""} ${type || ""}`}>
       {content}
     </button>
   );
