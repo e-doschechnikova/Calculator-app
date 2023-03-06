c33376e1e6a6f6d49d668a2415a4cae982a170ae
e-doschechnikova <evadrobakova@gmail.com>
2023-03-06 18:19:55 +0300

    fix styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Button.scss";
 
-// type ButtonPropsType = {
-//   value: string | Array<string>;
-//   callback: () => void;
-// };
+type ButtonPropsType = {
+  content: string;
+};
 
-export const Button = () => {
-  return <button className="Button">AC</button>;
+export const Button: React.FC<ButtonPropsType> = ({ content }) => {
+  return (
+    <button className={`Button ${content === "0" ? "zero" : ""}`}>
+      {content}
+    </button>
+  );
 };
