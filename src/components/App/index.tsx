67770944cd9495ad5ccffaed9692736a3323f2a4
e-doschechnikova <evadrobakova@gmail.com>
2023-03-06 20:23:17 +0300

    add logic for button

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,30 +4,79 @@ import "./App.scss";
 
 const App = () => {
   const [value, setValue] = useState<string>("0");
+
+  const handleButtonPress = (content: any) => () => {
+    // console.log("value", value);
+    const num = parseFloat(value);
+
+    if (content === "AC") {
+      setValue("0");
+      return;
+    }
+    if (content === "±") {
+      setValue((num * -1).toString());
+      return;
+    }
+
+    setValue(parseFloat(num + content).toString());
+  };
+
   return (
     <div className="App">
       <div className="top">17:38</div>
       <div className="display">{value}</div>
       <div className="buttons">
-        <Button content={"AC"} type={"function"} />
-        <Button content={"±"} type={"function"} />
-        <Button content={"%"} type={"function"} />
-        <Button content={"÷"} type={"operator"} />
-        <Button content={"7"} />
-        <Button content={"8"} />
-        <Button content={"9"} />
-        <Button content={"×"} type={"operator"} />
-        <Button content={"4"} />
-        <Button content={"5"} />
-        <Button content={"6"} />
-        <Button content={"−"} type={"operator"} />
-        <Button content={"1"} />
-        <Button content={"2"} />
-        <Button content={"3"} />
-        <Button content={"+"} type={"operator"} />
-        <Button content={"0"} />
-        <Button content={"."} />
-        <Button content={"="} type={"operator"} />
+        <Button
+          onButtonClick={handleButtonPress}
+          content={"AC"}
+          type={"function"}
+        />
+        <Button
+          onButtonClick={handleButtonPress}
+          content={"±"}
+          type={"function"}
+        />
+        <Button
+          onButtonClick={handleButtonPress}
+          content={"%"}
+          type={"function"}
+        />
+        <Button
+          onButtonClick={handleButtonPress}
+          content={"÷"}
+          type={"operator"}
+        />
+        <Button onButtonClick={handleButtonPress} content={"7"} />
+        <Button onButtonClick={handleButtonPress} content={"8"} />
+        <Button onButtonClick={handleButtonPress} content={"9"} />
+        <Button
+          onButtonClick={handleButtonPress}
+          content={"×"}
+          type={"operator"}
+        />
+        <Button onButtonClick={handleButtonPress} content={"4"} />
+        <Button onButtonClick={handleButtonPress} content={"5"} />
+        <Button onButtonClick={handleButtonPress} content={"6"} />
+        <Button
+          onButtonClick={handleButtonPress}
+          content={"−"}
+          type={"operator"}
+        />
+        <Button onButtonClick={handleButtonPress} content={"1"} />
+        <Button onButtonClick={handleButtonPress} content={"2"} />
+        <Button onButtonClick={handleButtonPress} content={"3"} />
+        <Button
+          onButtonClick={handleButtonPress}
+          content={"+"}
+          type={"operator"}
+        />
+        <Button onButtonClick={handleButtonPress} content={"0"} />
+        <Button onButtonClick={handleButtonPress} content={"."} />
+        <Button
+          onButtonClick={handleButtonPress}
+          content={"="}
+          type={"operator"}
+        />
       </div>
       <div className="bottom"></div>
     </div>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,11 +4,20 @@ import "./Button.scss";
 type ButtonPropsType = {
   content: string;
   type?: string;
+
+  onButtonClick: (content: any) => () => void;
 };
 
-export const Button: React.FC<ButtonPropsType> = ({ content, type }) => {
+export const Button: React.FC<ButtonPropsType> = ({
+  content,
+  type,
+  onButtonClick,
+}) => {
   return (
-    <button className={`Button ${content === "0" ? "zero" : ""} ${type || ""}`}>
+    <button
+      className={`Button ${content === "0" ? "zero" : ""} ${type || ""}`}
+      onClick={onButtonClick(content)}
+    >
       {content}
     </button>
   );
